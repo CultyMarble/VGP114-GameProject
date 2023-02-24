d99de53379c0e4806a8bdb77753f86b4887a7a5b
Tin Ly <lytranhoantin811@gmail.com>
2023-02-24 04:39:28 -0800

    Adjust iter2

--- SoftwareDevelopmentProject/Assets/SCRIPTS/ScriptableObjectBase/SO_LIST/SO_DefenseBuildingList.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO/DefenseBuildingList", fileName = "New Defense Building List")]
-public class SO_DefenseBuildingList : ScriptableObject
-{
-    public List<SO_DefenseBuildingType> ListOfDefenseBuilding;
-}
--- SoftwareDevelopmentProject/Assets/SCRIPTS/ScriptableObjectBase/SO_LIST/SO_ResourceTypeList.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO/ResourceTypeList", fileName = "New List")]
-public class SO_ResourceTypeList : ScriptableObject
-{
-    public List<SO_ResourceType> ListOfResourceType;
-}
