76ab159d0ebc62d6d6d4e88c1ed9934db69b362a
Tin Ly <lytranhoantin811@gmail.com>
2023-02-24 04:38:35 -0800

    adjust main

--- SoftwareDevelopmentProject/Assets/SCRIPTS/Resource/ResourceBuildingPref.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class ResourceBuildingPref : MonoBehaviour
-{
-    public SO_ResourceBuildingType buildingPreference;
-}
--- SoftwareDevelopmentProject/Assets/SCRIPTS/ScriptableObjectBase/SO_LIST/SO_ResourceBuildingList.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO/ResourceBuildingList", fileName = "New Resource Building List")]
-public class SO_ResourceBuildingList : ScriptableObject
-{
-    public List<SO_ResourceBuildingType> ListOfResourceBuilding;
-}
